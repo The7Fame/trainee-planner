faf92153e26e42c30beb4bc04ede29b0847aefe8
DD <dd@m.m>
2023-08-11 18:47:29 +0300

    Add User model and make seeds

--- db/seeds/development.rb
+++ db/seeds/development.rb
@@ -1,3 +1,13 @@
 # frozen_string_literal: true
 
 %w[Personal Work Vacation].each { |category| Category.find_or_create_by!(name: category) }
+
+5.times do |index|
+  User.create_or_find_by!(
+    first_name: "User#{index + 1}",
+    email: "test#{index + 1}@test.com",
+    password: "password#{index + 1}"
+  )
+rescue ActiveRecord::RecordInvalid => e
+  Rails.logger.debug e.message
+end
