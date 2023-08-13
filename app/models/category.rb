46c9770edca879a2eb5817b208df3130ea46d227
DD <dd@m.m>
2023-08-13 08:31:48 +0300

    Add controller and views for Category model, add CategoriesServices

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Category < ApplicationRecord
+  paginates_per 15
+
   validates :name, presence: true, uniqueness: { case_sensitive: false }
 
   has_many :user_categories, dependent: :destroy
