6fba315fd784ceb699107b6862f8f8ce5fdbde21
DD <dd@m.m>
2023-08-21 15:30:37 +0300

    Add seeds and tests for Event model

--- db/seeds/development.rb
+++ db/seeds/development.rb
@@ -1,11 +1,20 @@
 # frozen_string_literal: true
 
-%w[Personal Work Vacation].each { |category| Category.find_or_create_by!(name: category) }
+categories = %w[Personal Work Vacation].map { |category| Category.find_or_create_by!(name: category) }
 
-5.times do |index|
+users = 5.times.map do |index|
   User.find_or_create_by!(email: "user_#{index}@example.com") do |user|
     user.first_name = Faker::Name.first_name
     user.last_name = Faker::Name.last_name
     user.password = 'password'
   end
+end
+
+10.times do
+  Event.create!(
+    name: Faker::Lorem.word,
+    event_date: Faker::Date.between(from: DateTime.now.tomorrow, to: 1.year.from_now),
+    category: categories.sample,
+    user: users.sample
+  )
 end
