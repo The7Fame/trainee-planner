24831ce973b0e89cdd8b33d1bedee3f0645429ed
DD <dd@m.m>
2023-08-22 16:36:10 +0300

    Add HomeController and add locales

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  scope '(:locale)', locale: /#{I18n.available_locales.join("|")}/ do
+    root 'home#index'
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
