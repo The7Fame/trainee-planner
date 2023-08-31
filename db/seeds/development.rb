c38833a70ee2cbc4144159e8901775bce14293c5
DD <dd@m.m>
2023-08-31 12:13:53 +0300

    modify seeds for User model

--- db/seeds/development.rb
+++ db/seeds/development.rb
@@ -3,11 +3,9 @@
 %w[Personal Work Vacation].each { |category| Category.find_or_create_by!(name: category) }
 
 5.times do |index|
-  User.create_or_find_by!(
-    first_name: Faker::Name.first_name,
-    email: "user_#{index + 1}@example.com",
-    password: 'password'
-  )
-rescue ActiveRecord::RecordInvalid => e
-  Rails.logger.debug e.message
+  User.find_or_create_by!(email: "user_#{index}@example.com") do |user|
+    user.first_name = Faker::Name.first_name
+    user.last_name = Faker::Name.last_name
+    user.password = 'password'
+  end
 end
